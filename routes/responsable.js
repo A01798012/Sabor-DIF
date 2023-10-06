ea76756276e236d3797823f18d0ce20f6de78b13
Ubuntu <ubuntu@ip-172-31-21-162.ec2.internal>
2023-10-06 00:02:16 +0000

    login de responsable

--- routes/responsable.js
+++ routes/responsable.js
@@ -4,16 +4,17 @@ const router = express.Router();
 const mariadb = require("mariadb");
 const pool = mariadb.createPool({
   host: 'localhost',
-  user: 'your_db_user',
-  password: 'your_db_password',
-  database: 'your_db_name',
+  user: 'root',
+  password: 'pepe',
+  database: 'comedor',
 });
 //TODO login
 router.post("/login", async function(req, res){
   try {
-    const {nombreComedor, pswd} = req.body
+    const {nombreComedor, pswd} = req.body;
+console.log(req.body);
     const connection = await pool.getConnection();
-    const rows = await connection.query("CALL loginResponsable(?,?)", [nombreComedor, pswd]);
+    const rows = await connection.query("SELECT loginResponsable(?,?)", [nombreComedor, pswd]);
     connection.release();
     console.log(rows[0]);
     res.status(201).send({message: "OK", access: rows[0]});
